4b05dd5fb1af04013912db823d5000e0445cebad
Aliamzzz <ajangali2000@gmail.com>
2024-05-19 14:28:09 +0330

    Player
    linked camera with player and added movement to player.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float playerSpeed;
+    private Transform _transform;
     void Start()
     {
-        
+        _transform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            _transform.position += new Vector3(0, playerSpeed, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            _transform.position += new Vector3(0, -playerSpeed, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            _transform.position += new Vector3(-playerSpeed, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            _transform.position += new Vector3(playerSpeed, 0, 0) * Time.deltaTime;
+        }
     }
 }
